12b1c5593dd04823b7b85ad75ca9523f3729c6ff
Paolo Vasta <nitro.wags.8m@icloud.com>
2026-05-12 09:45:31 +0100

    Create CodeDisclaimer.swift

---
+++ CodeDisclaimer.swift
@@ -0,0 +1,3 @@
+My app is not open source.
+Due to concerns over code and idea theft, where others use AI to refactor and repackage content for commercial gain or false copyright claims, I am compelled to remove my app's code from this platform.
+If you wish to see the code of my EV-Companion application, please contact me as per main page contact details. Thank you.
